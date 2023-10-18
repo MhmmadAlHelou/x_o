0df9e00c3fa717d60b6d7378bf8e036f3f365a58
MhmmadAlHelou <Muhammad alhelou@xxHxx>
2023-10-18 16:34:41 +0300

    finesh the app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:x_o/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF00061a),
+        shadowColor: const Color(0xFF001456),
+        splashColor: const Color(0xFF4169e8),
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: const HomePage(),
+    );
+  }
+}
